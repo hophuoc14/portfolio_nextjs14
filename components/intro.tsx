a3533c2d8c5cdd0ecc233d455901df5b55364f2a
anonyDev <phuochh@nortstudio.vn>
2024-04-15 00:31:15 +0700

    update profile third party

--- components/intro.tsx
+++ components/intro.tsx
@@ -147,7 +147,7 @@ export default function Intro() {
 				
 				<a
 					className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-					href="https://linkedin.com"
+					href="https://www.linkedin.com/in/ph%C6%B0%E1%BB%9Bc-h%E1%BB%93-b16399267/"
 					target="_blank"
 				>
 					<BsLinkedin/>
@@ -155,7 +155,7 @@ export default function Intro() {
 				
 				<a
 					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-					href="https://github.com"
+					href="https://github.com/phuocAnonydev14"
 					target="_blank"
 				>
 					<FaGithubSquare/>
